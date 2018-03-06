dcc8c256c1993a3dd00a3746a2d32c790a5182b2
donglaiw <donglai@coxgpu06.rc.fas.harvard.edu>
2018-03-06 16:13:27 -0500

    remove folder idm

--- idm/cpp_idm/idm_main.h
+++
@@ -1,6 +0,0 @@
-#include <float.h>
-#include <stdint.h>
-#include <stdio.h>
-
-void idm_dist(float *img1, float *img2, float *out, 
-        int im_chan, int im_col, int im_row, int patch_sz, int warp_sz);
